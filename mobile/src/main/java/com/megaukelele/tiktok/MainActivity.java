863727314fbb86016a84c3de08225264fd52ef79
Elizabeth Hagearty <elizabeth@glassymedia.com>
2015-11-30 08:29:09 -0800

    added image views to the mobile layouts, set onclick for main activity

--- mobile/src/main/java/com/megaukelele/tiktok/MainActivity.java
+++ mobile/src/main/java/com/megaukelele/tiktok/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
     public Button settings;
@@ -15,6 +16,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Button btn_go=(Button)findViewById(R.id.settings);
         btn_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Log.i("clicks", "You Clicked B1");
+                Intent i = new Intent(
+                        MainActivity.this,
+                        SettingsActivity.class);
+                startActivity(i);
+            }
+        });
+        ImageButton btn_temp=(ImageButton)findViewById(R.id.imagesettings);
+        btn_temp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
